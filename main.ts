d144827bebd2a38f35695b9c5efa9d067779cff7
vmeng321 <vmeng321@users.noreply.github.com>
2021-06-24 19:38:58 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # #
+        . . . # .
+        . . . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # #
+        . . . # .
+        . . . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . . # . .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . #
+        . # # # .
+        . # . . #
+        . # # # .
+        `)
+})
